f10f5d97c28b18acf82a5cf7835fd6e70c6d8720
Jan Pfeifer <pfeifer@gmail.com>
2024-12-19 10:46:35 +0100

    Fixed coverage script. Updated coverage.

--- cmd/run_coverage.sh
+++ cmd/run_coverage.sh
@@ -2,6 +2,6 @@
 
 # Run this from the root of gopjrt repository to generate docs/coverage.out with the coverage data.
 
-PACKAGE_COVERAGE="./pjrt ./xlabuilder"
-go test -v -cover -coverprofile docs/coverage.out -coverpkg ${PACKAGE_COVERAGE}
+PACKAGE_COVERAGE="github.com/gomlx/gopjrt/pjrt,github.com/gomlx/gopjrt/xlabuilder"
+go test -cover -coverprofile docs/coverage.out -coverpkg="${PACKAGE_COVERAGE}" ./... -test.count=1 -test.short
 go tool cover -func docs/coverage.out -o docs/coverage.out

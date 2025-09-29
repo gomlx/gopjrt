f1566694a9acc87e97f96d5384799160abe39b6f
Jan Pfeifer <pfeifer@gmail.com>
2025-09-29 20:02:29 +0200

    gopjrt_installer working for linux and amazonlinux already.

--- cmd/gopjrt_installer/linux.go
+++ cmd/gopjrt_installer/linux.go
@@ -5,9 +5,14 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"strings"
 
+	"github.com/charmbracelet/huh/spinner"
 	"github.com/pkg/errors"
+	"k8s.io/klog/v2"
 )
 
 // LinuxValidateVersion checks whether the linux version selected in -version exists.
@@ -17,18 +22,32 @@ func LinuxValidateVersion() error {
 		return nil
 	}
 
-	assets, err := LinuxDownloadReleaseAssets(*flagVersion)
+	_, err := LinuxGetDownloadURL(*flagPlugin, *flagVersion)
+	return err
+}
+
+// LinuxGetDownloadURL returns the download URL for the given version and plugin.
+func LinuxGetDownloadURL(plugin, version string) (string, error) {
+	assets, err := LinuxDownloadReleaseAssets(version)
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	wantAsset := fmt.Sprintf("gomlx_xlabuilder_%s.tar.gz", *flagPlugin)
+	var wantAsset string
+	switch plugin {
+	case "linux":
+		wantAsset = "gomlx_xlabuilder_linux_amd64.tar.gz"
+	case AmazonLinux:
+		wantAsset = "gomlx_xlabuilder_linux_amd64_amazonlinux.tar.gz"
+	default:
+		return "", errors.Errorf("version validation not implemented for plugin %q in version %s", plugin, version)
+	}
 	for _, asset := range assets {
-		if strings.Contains(asset, wantAsset) {
-			return nil
+		if strings.HasSuffix(asset, wantAsset) {
+			return asset, nil
 		}
 	}
-	return errors.Errorf("Plugin %q version %q doesn't seem to have the required asset (%q) -- assets found: %v", *flagPlugin, *flagVersion, wantAsset, assets)
+	return "", errors.Errorf("Plugin %q version %q doesn't seem to have the required asset (%q) -- assets found: %v", plugin, version, wantAsset, assets)
 }
 
 // LinuxDownloadReleaseAssets downloads the list of assets available for the given Gopjrt release version.
@@ -71,4 +90,101 @@ func LinuxDownloadReleaseAssets(version string) ([]string, error) {
 	}
 
 	return urls, nil
+}
+
+// LinuxInstall the assets on the target directory.
+func LinuxInstall() error {
+	assetURL, err := LinuxGetDownloadURL(*flagPlugin, *flagVersion)
+	if err != nil {
+		return err
+	}
+
+	// Create the target directory.
+	installPath := ReplaceTildeInDir(*flagPath)
+	if err := os.MkdirAll(installPath, 0755); err != nil {
+		return errors.Wrap(err, "failed to create install directory")
+	}
+
+	// Download the asset to a temporary file
+	tmpFile, err := os.CreateTemp("", "gopjrt_*.tar.gz")
+	if err != nil {
+		return errors.Wrap(err, "failed to create temporary file")
+	}
+	if !klog.V(1).Enabled() {
+		defer os.Remove(tmpFile.Name())
+	}
+
+	var bytesDownloaded int64
+	spinnerErr := spinner.New().
+		Title(fmt.Sprintf("Downloading %s….", assetURL)).
+		Action(func() {
+			var resp *http.Response
+			resp, err = http.Get(assetURL)
+			if err != nil {
+				err = errors.Wrapf(err, "failed to download asset %s", assetURL)
+				return
+			}
+			defer resp.Body.Close()
+
+			bytesDownloaded, err = io.Copy(tmpFile, resp.Body)
+			if err != nil {
+				err = errors.Wrapf(err, "failed to write asset %s to temporary file %s", assetURL, tmpFile.Name())
+				return
+			}
+			tmpFile.Close()
+		}).
+		Run()
+	if spinnerErr != nil {
+		return errors.Wrapf(spinnerErr, "failed run asset download from %s", assetURL)
+	}
+	if err != nil {
+		return err
+	}
+
+	// Print the downloaded size
+	bytesStr := ""
+	switch {
+	case bytesDownloaded < 1024:
+		bytesStr = fmt.Sprintf("%d B", bytesDownloaded)
+	case bytesDownloaded < 1024*1024:
+		bytesStr = fmt.Sprintf("%.1f KB", float64(bytesDownloaded)/1024)
+	case bytesDownloaded < 1024*1024*1024:
+		bytesStr = fmt.Sprintf("%.1f MB", float64(bytesDownloaded)/(1024*1024))
+	default:
+		bytesStr = fmt.Sprintf("%.1f GB", float64(bytesDownloaded)/(1024*1024*1024))
+	}
+	fmt.Printf("- Downloaded %s to %s\n", bytesStr, tmpFile.Name())
+
+	// Create a temporary file to store the extracted files list.
+	listFile, err := os.CreateTemp("", "gopjrt_list_files.*")
+	if err != nil {
+		return errors.Wrap(err, "failed to create list file")
+	}
+	if !klog.V(1).Enabled() {
+		defer os.Remove(listFile.Name())
+	}
+
+	// Extract files
+	fmt.Printf("- Extracting files in %s to %s\n", tmpFile.Name(), installPath)
+	cmd := exec.Command("tar", "xvzf", tmpFile.Name(), "-C", installPath)
+	cmd.Stdout = listFile
+	klog.V(1).Infof("Running command: %v > %s", cmd.Args, listFile.Name())
+	if err := cmd.Run(); err != nil {
+		return errors.Wrap(err, "failed to extract files")
+	}
+	_ = listFile.Close()
+
+	// Remove older version using dynamically linked library
+	oldLib := filepath.Join(installPath, "lib/libgomlx_xlabuilder.so")
+	os.Remove(oldLib)
+
+	// Read and print the list of extracted files
+	fmt.Println("- Extracted files:")
+	fileContents, err := os.ReadFile(listFile.Name())
+	if err != nil {
+		return errors.Wrap(err, "failed to read list of extracted files")
+	}
+	fmt.Print(string(fileContents))
+
+	return nil
 }
--- cmd/gopjrt_installer/main.go
+++ cmd/gopjrt_installer/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/pkg/errors"
@@ -19,8 +20,8 @@ var (
 			"The CUDA plugins will download the PJRT and Nvidia drivers included in Jax distribution for "+
 			"pypi.org (but it doesn't use Python to install them)", strings.Join(pluginValues, ", ")))
 
-	pathSuggestions = []string{"/usr/local/lib/", "~/.local/lib"}
-	flagPath        = flag.String("path", "~/.local/lib",
+	pathSuggestions = []string{"/usr/local/", "~/.local"}
+	flagPath        = flag.String("path", "~/.local",
 		fmt.Sprintf("Path where the PJRT plugin will be installed. "+
 			"It will create a sub-directory gomlx/prjt for the PJRT plugin, and in case of CUDA plugins, gomlx/nvidia for "+
 			"Nvidia's matching drivers. Suggestions: %s. It will require the adequate privileges if installing in system directories.",
@@ -31,6 +32,7 @@ var (
 )
 
 func main() {
+	klog.InitFlags(nil)
 	flag.Parse()
 
 	if *flagPlugin == "" || *flagPath == "" || *flagVersion == "" {
@@ -38,7 +40,8 @@ func main() {
 			{Name: "Plugin to install", Flag: flag.CommandLine.Lookup("plugin"), Values: pluginValues, CustomValues: false},
 			{Name: "Plugin version", Flag: flag.CommandLine.Lookup("version"), Values: []string{"latest"}, CustomValues: true,
 				ValidateFn: ValidateVersion},
-			{Name: "Path where to install", Flag: flag.CommandLine.Lookup("path"), Values: pathSuggestions, CustomValues: true},
+			{Name: "Path where to install", Flag: flag.CommandLine.Lookup("path"), Values: pathSuggestions, CustomValues: true,
+				ValidateFn: ValidatePathPermission},
 		}
 		err := Interact(os.Args[0], questions)
 		if err != nil {
@@ -49,7 +52,18 @@ func main() {
 	pluginName := *flagPlugin
 	version := *flagVersion
 	installPath := ReplaceTildeInDir(*flagPath)
-	fmt.Printf("Installing PJRT plugin %q@%q to %q \n", pluginName, version, installPath)
+	fmt.Printf("Installing PJRT plugin %s@%s to %s:\n", pluginName, version, installPath)
+
+	var err error
+	switch pluginName {
+	case "linux", AmazonLinux:
+		err = LinuxInstall()
+	default:
+		err = errors.Errorf("plugin %q installation not implemented", pluginName)
+	}
+	if err != nil {
+		klog.Fatal(err)
+	}
 }
 
 func ValidateVersion() error {
@@ -57,4 +71,48 @@ func ValidateVersion() error {
 		return LinuxValidateVersion()
 	}
 	return errors.Errorf("version validation not implemented for plugin %q", *flagPlugin)
+}
+
+func ValidatePathPermission() error {
+	installPath := ReplaceTildeInDir(*flagPath)
+
+	// Check permissions in lib/ and include/ subdirectories
+	dirsToCheck := []string{
+		filepath.Join(installPath, "lib/"),
+		filepath.Join(installPath, "include/gomlx"),
+	}
+
+	for _, dir := range dirsToCheck {
+		// Check if the directory exists: if it doesn't exist, try to create it in the parent directories
+		_, err := os.Stat(dir)
+		if err != nil {
+			// If the directory doesn't exist, try parent directories
+			parent := dir
+			for {
+				parent = filepath.Dir(parent)
+				if parent == "/" || parent == "." {
+					return errors.New("could not find an existing parent directory")
+				}
+				if _, err := os.Stat(parent); err == nil {
+					dir = parent
+					break
+				}
+			}
+		}
+
+		// Try to create a temporary file to verify write permissions
+		testFile := filepath.Join(dir, ".gopjrt_write_test")
+		f, err := os.Create(testFile)
+		if err != nil {
+			return errors.Wrapf(err, "no write permission in directory %q, do you need \"sudo\" ?", dir)
+		}
+		f.Close()
+
+		// Clean up test file
+		if err = os.Remove(testFile); err != nil {
+			return errors.Wrapf(err, "failed to remove test file %q", testFile)
+		}
+	}
+
+	return nil
 }
